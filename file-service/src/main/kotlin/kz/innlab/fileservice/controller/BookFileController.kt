3e6d1b2d2c42ce41cacdd5071ced2b1414e01f01
KONGRATBAYEV NURZHAN <kongratbayevn@gmail.com>
2023-09-25 11:49:46 +0600

    file service callback

--- file-service/src/main/kotlin/kz/innlab/fileservice/controller/BookFileController.kt
+++ file-service/src/main/kotlin/kz/innlab/fileservice/controller/BookFileController.kt
@@ -44,6 +44,34 @@ class BookFileController {
 
         return null
     }
+    @PostMapping("/onlyoffice-callback")
+    fun handleCallback(@RequestBody body: String): ResponseEntity<String> {
+        // Your callback handling logic here
+        println("uywberpigwbherg")
+        // Parse the JSON data from OnlyOffice (assuming it's in JSON format)
+//        val jsonObj: JSONObject = JSONParser().parse(body) as JSONObject
+//
+//        if (jsonObj["status"] as Long == 2L) {
+//            val downloadUri = jsonObj["url"] as String
+//            val url = URL(downloadUri)
+//            val connection = url.openConnection() as HttpURLConnection
+//            val stream = connection.inputStream
+//            val savedFile = File("pathForSave")
+//            FileOutputStream(savedFile).use { out ->
+//                var read: Int
+//                val bytes = ByteArray(1024)
+//                while (stream.read(bytes).also { read = it } != -1) {
+//                    out.write(bytes, 0, read)
+//                }
+//                out.flush()
+//            }
+//            connection.disconnect()
+//        }
+
+        // Return a response (for example, a success message)
+        return ResponseEntity.ok("{\"error\":0}")
+    }
+
 
     @PostMapping("/create")
     @PreAuthorize("#oauth2.hasScope('server')")
@@ -61,6 +89,8 @@ class BookFileController {
         // Return a response (for example, a success message)
         return ResponseEntity.ok("{\"error\":0}")
     }
+
+
 
     @PostMapping("/sketchfab/upload")
     @PreAuthorize("isAuthenticated()")
